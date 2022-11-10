a7d3881da62edeb5f85b03d45b143c5cd96fed94
jabrahamec <86816067+jabrahamec@users.noreply.github.com>
2022-11-10 17:04:45 -0500

    cambio en sw y manistest

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ;
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
+const CACHE_NAME = 'v1_cache_garita_proyecto',
   urlsToCache = [
     './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
