73f46d8c0fd81dce0034598120d84e638cdce678
jabrahamec <86816067+jabrahamec@users.noreply.github.com>
2022-11-10 17:22:25 -0500

    cambio en GaritaProyecto.png

--- sw.js
+++ sw.js
@@ -7,13 +7,14 @@ const CACHE_NAME = 'v1_cache_garita_proyecto',
     'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
     'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
-    './style.css',
+    './css/estilos.css',
     './script.js',
-    './img/ProgramadorFitness.png',
+    './js/categorias.js',
+    './img/GaritaProyecto.png',
     './img/favicon.png'
   ]
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+//durante la fase de instalacion, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
