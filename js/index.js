1257de9e52794ab9f312b59521e9f7fbccaf3178
jabrahamec <86816067+jabrahamec@users.noreply.github.com>
2022-11-10 13:28:42 -0500

    nex

--- js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let sw = null;
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(swRegistered => {
-        console.log("[ServiceWorker**] - Registered");
-        sw = swRegistered;
-    });
-}
